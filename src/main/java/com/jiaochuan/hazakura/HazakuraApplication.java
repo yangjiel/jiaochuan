ff1142dc6097af8bbe4f459cca154c59157344a0
Jason Liu <jasonliuengr@gmail.com>
2020-05-23 16:19:15 -0700

    Initial commit
    Setup the project

---
+++ src/main/java/com/jiaochuan/hazakura/HazakuraApplication.java
@@ -0,0 +1,13 @@
+package com.jiaochuan.hazakura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazakuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazakuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiaochuan/hazakura/HazakuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiaochuan.hazakura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazakuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
