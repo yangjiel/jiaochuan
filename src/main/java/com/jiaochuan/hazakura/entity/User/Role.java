f7460a51f945c0e55451dead8732e0c1c426e6c2
Peter Tan <pj.t@outlook.com>
2020-06-14 19:44:43 -0700

    Refactor entities

---
+++ src/main/java/com/jiaochuan/hazakura/entity/User/Role.java
@@ -0,0 +1,25 @@
+package com.jiaochuan.hazakura.entity.User;
+
+public enum Role {
+    STAFF_CUSTOMER_SERVICE("客服人员"),
+    STAFF_PROCUREMENT("采购人员"),
+    STAFF_QUALITY_CONTROL("质检人员"),
+    STAFF_INVENTORY("库管人员"),
+    STAFF_TECHNICAL("技术人员"),
+
+    MANAGER_AFTER_SALES("售后经理"),
+    MANAGER_PROCEREMENT("采购经理"),
+    MANAGER_QUALITY_CONTROL("质检经理"),
+
+    DIRECTOR_AFTER_SALES("售后主任"),
+    DIRECTOR_TECHNICAL("技术总监"),
+
+    ENGINEER_AFTER_SALES("售后工程师");
+
+
+    public final String roleDescription;
+
+    private Role(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
+}
