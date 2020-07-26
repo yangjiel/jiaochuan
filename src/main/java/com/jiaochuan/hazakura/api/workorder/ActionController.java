10343412456a57f71594b5bddc2c6b4fcd0509e0
Jason Liu <jasonliuengr@gmail.com>
2020-07-26 13:45:59 -0700

    Add APIs for action and part list

---
+++ src/main/java/com/jiaochuan/hazakura/api/workorder/ActionController.java
@@ -0,0 +1,38 @@
+package com.jiaochuan.hazakura.api.workorder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jiaochuan.hazakura.entity.workorder.ActionEntity;
+import com.jiaochuan.hazakura.exception.AppException;
+import com.jiaochuan.hazakura.service.ActionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/action")
+public class ActionController {
+    @Autowired
+    private ActionService actionService;
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> createAction(@RequestBody String jsonRequest) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ActionEntity actionEntity = objectMapper.convertValue(jsonRequest, ActionEntity.class);
+            actionService.createAction(actionEntity);
+            return ResponseEntity.ok().build();
+        } catch (AppException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("服务器出现错误，请与管理员联系。内部错误：" + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
+    }
+}
