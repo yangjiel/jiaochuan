ae530da428aa39384f38200c2f0cec2ce773c21b
Jason Liu <jasonliuengr@gmail.com>
2020-07-26 13:45:59 -0700

    Add APIs for action and part list

---
+++ src/main/java/com/jiaochuan/hazakura/api/workorder/PartListController.java
@@ -0,0 +1,38 @@
+package com.jiaochuan.hazakura.api.workorder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jiaochuan.hazakura.entity.workorder.PartListEntity;
+import com.jiaochuan.hazakura.exception.AppException;
+import com.jiaochuan.hazakura.service.PartListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/part_list")
+public class PartListController {
+    @Autowired
+    private PartListService partListService;
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> createPartList(@RequestBody String jsonRequest) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            PartListEntity partListEntity = objectMapper.convertValue(jsonRequest, PartListEntity.class);
+            partListService.createPartList(partListEntity);
+            return ResponseEntity.ok().build();
+        } catch (AppException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("服务器出现错误，请与管理员联系。内部错误：" + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/jiaochuan/hazakura/service/ActionService.java
@@ -0,0 +1,35 @@
+package com.jiaochuan.hazakura.service;
+
+import com.jiaochuan.hazakura.entity.workorder.ActionEntity;
+import com.jiaochuan.hazakura.exception.AppException;
+import com.jiaochuan.hazakura.jpa.WorkOrder.ActionRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Field;
+
+@Service
+public class ActionService {
+    @Autowired
+    private ActionRepository actionRepository;
+
+    public void createAction(ActionEntity actionEntity) throws AppException, Exception {
+        // Check if required fields are not empty
+        try {
+            for (Field field : actionEntity.getClass().getDeclaredFields()) {
+                if (!field.trySetAccessible()) {
+                    throw new AppException("创建用户时无法取得ActionEntity的反射访问权限，其成员变量无法通过反射访问。");
+                }
+
+                if (field.getType() == String.class && StringUtils.isBlank((String) field.get(actionEntity))) {
+                    throw new Exception("必填项" + field.getName() + "不能为空。");
+                } else if (field.get(actionEntity) == null) {
+                    throw new Exception("必填项" + field.getName() + "不能为空。");
+                }
+            }
+        } catch (IllegalAccessException e) {
+            throw new AppException("创建用户时无法取得ActionEntity的反射访问权限，其成员变量无法通过反射访问。");
+        }
+    }
+}
